979e7089ee5ee71f2ba6151d1860151a2bb55b06
yingpengsha <yingpengsha@kuaishou.com>
2022-07-28 21:11:33 +0800

    remove useless code

--- core/client/useLoaderData.ts
+++ core/client/useLoaderData.ts
@@ -4,7 +4,6 @@ import { FiremixPageDataContext, GLOBAL_DATA_ID } from '../common/FiremixContext
 export default function useLoaderData <T> (): T {
   const pageData = useContext(FiremixPageDataContext)
   if (typeof window !== 'undefined') {
-    // @ts-ignore
     return window?.[GLOBAL_DATA_ID] as any
   }
   return pageData
